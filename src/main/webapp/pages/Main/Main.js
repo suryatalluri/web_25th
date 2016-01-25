54fb42a0543ccdb5b148421f84deca2f7f8172a3
Surya null <surya.talluri+1@wavemaker.com>
2016-01-25 08:34:27 +0000

    New files added - [hrdb_procedure.json,User.java,DepartmentService.java,DepartmentServiceImpl.java,UserService.java,UserServiceImpl.java,hrdb-queries.hbm.xml,VacationController.java,VacationDao.java,hrdb-procedures.mappings.json,DepartmentDao.java,ProcedureExecutionController.java,hrdb_vk.hbm.xml,db-connection-settings.json,VacationServiceImpl.java,HrdbProcedureExecutorServiceImpl.java,Vacation.java,EmployeeServiceImpl.java,hrdb_API.json,Department.java,HrdbQueryExecutorService.java,servicedef.xml,hrdb.properties,DepartmentController.java,hrdb.log,hrdb_published_dataModel.json,Employee.java,hrdb_query.json,hrdb.spring.xml,UserController.java,HrdbProcedureExecutorService.java,EmployeeDao.java,hrdb.script,QueryExecutionController.java,EmployeeService.java,hrdb.properties,HrdbQueryExecutorServiceImpl.java,VacationService.java,UserDao.java,EmployeeController.java] Files modified - [Main.html,production.properties,Main.js,project-services.xml,page.min.html,wmProperties.js,app.variables.json,development.properties] Refactored files - [] Fixed bugs - [] Removed files - []

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -1,21 +1,42 @@
 Application.$controller("MainPageController", ["$scope", function ($scope) {
     "use strict";
 
-    /* perform any action on the variables within this block(on-page-load) */
+    /* perform any action with the variables inside this block(on-page-load) */
     $scope.onPageVariablesReady = function () {
         /*
          * variables can be accessed through '$scope.Variables' property here
-         * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
-         * $scope.Variables.loggedInUser.getData()
+         * e.g. $scope.Variables.staticVariable1.getData()
          */
     };
-    /* perform any action on widgets within this block */
+    
+    /* perform any action with widgets inside this block */
     $scope.onPageReady = function () {
         /*
          * widgets can be accessed through '$scope.Widgets' property here
-         * e.g. to get value of text widget named 'username' use following script
-         * '$scope.Widgets.username.datavalue'
+         * e.g. $scope.Widgets.byId(), $scope.Widgets.byName()or access widgets by $scope.Widgets.widgetName
          */
     };
 
 }]);
+
+
+Application.$controller("livefilter1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("grid1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("liveform1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
